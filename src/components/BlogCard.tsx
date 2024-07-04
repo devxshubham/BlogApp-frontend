2dfc8be33ecddcfda3f8a13c5d6d003c537dbea2
devxshubham <devshubhamcode@gmail.com>
2024-07-04 13:43:23 +0530

    all blog page and blogCard

---
+++ src/components/BlogCard.tsx
@@ -0,0 +1,44 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+interface blogCardType {
+    id : string,
+    authorId : string,
+    content : string,
+    title :string,
+}
+
+function BlogCard({id,authorId,content,title} : blogCardType) {
+    const [user,setUser] = useState({
+        name : "",
+        id : "",
+    })
+
+    useEffect( () => {
+        async function getUser(){
+            const res = await axios.get(import.meta.env.VITE_BACKEND_URL+"/user/"+authorId);
+            setUser(res.data);
+        }
+        getUser();
+    },[authorId])
+
+    return <div className="flex flex-col gap-4" key={id}>
+        <div className="flex gap-3 items-center">
+            <Avatar/>
+            <div className="text-[15px]  font-semibold">{user.name}</div>
+            <div className="h-1 w-1  rounded-full bg-gray-500"></div>
+        </div>
+        <div className="text-[23px] font-bold">{title}</div>
+        <div className="font-serif">{content.slice(0,280)+"....."}</div>
+        <div>{authorId}</div>
+        <div>post:{id}</div>
+    </div>;
+}
+
+function Avatar(){
+    return <div className="relative w-8 h-8 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+    <svg className="absolute w-10 h-10 text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
+</div>
+}
+
+export default BlogCard;
--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,5 +1,48 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import BlogCard from "../components/BlogCard";
+
 function Blog() {
-    return ( <div>Blog</div> );
+  const [blogs, setBlogs] = useState([
+    {
+      id: "123",
+      authorId: "97",
+      title: "Why dogs are the best?",
+      content: `Dogs have a strong sense of smell and are known to never forget anyone they encounter because of this characteristic.Highly intelligent, dogs have the capability of expressing their joy and happiness by wagging their tails.They are known to be the most loyal of animals. Dogs can sense your pain and can be your best friend. They can sense emotions, and when you are sad, they become sad and when you are happy, they are happy.The dog is a simple animal that shows no complex characteristics. Dogs are selfless animals and they do not have any extraordinary requirements. They look for little care and affection. A dog becomes a good companion for its owner.Dogs are known to be one of the most faithful and loyal species. Dogs need good treatment and good care from us humans, and they are happy. `,
+    },
+  ]);
+
+  useEffect(() => {
+    async function getBlogs() {
+      const res = await axios.get(
+        import.meta.env.VITE_BACKEND_URL + "/blog/bulk",
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: localStorage.getItem("jwt_token"),
+          },
+        }
+      );
+      console.log(res.data);
+      setBlogs(res.data);
+    }
+    getBlogs();
+  }, []);
+  return (
+    <main className="flex flex-col gap-14 items-center justify-center mx-auto max-w-[700px]">
+      {blogs.map((blog) => {
+        return (
+          <BlogCard
+            key={blog.id}
+            id={blog.id}
+            authorId={blog.authorId}
+            title={blog.title}
+            content={blog.content}
+          />
+        );
+      })}
+    </main>
+  );
 }
 
 export default Blog;
