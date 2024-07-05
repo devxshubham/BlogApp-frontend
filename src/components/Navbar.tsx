a8a2680bab55f816d7b246b44d5c8cf098dcea87
devxshubham <devshubhamcode@gmail.com>
2024-07-06 02:20:48 +0530

    .

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { useLocation, useNavigate, useParams } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import logo from "../assets/logo-medium.png";
 
 import { TfiWrite } from "react-icons/tfi";
